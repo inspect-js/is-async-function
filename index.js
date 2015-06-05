a7ce00d537bf420338b91fee00eb6893d14952bf
tunnckoCore <mameto_100@mail.bg>
2015-06-05 05:47:35 +0300

    refactor

--- index.js
+++ index.js
@@ -7,7 +7,11 @@
 
 'use strict'
 
-module.exports = function isAsyncFn (fn) {
-  var fnStr = String(fn).slice(8, 150)
+module.exports = function isAsyncFn (fn, max) {
+  if (typeof fn !== 'function') {
+    throw new TypeError('is-async-function expect a function')
+  }
+
+  var fnStr = fn.toString().slice(8, Number(max) || 150)
   return fnStr.indexOf('callback') !== -1 || fnStr.indexOf('cb') !== -1
 }
