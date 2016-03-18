27b21bf76254635d6c5c18f896b6f151938ae810
tunnckoCore <mameto_100@mail.bg>
2016-03-18 08:44:09 +0200

    run update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*!
  * is-async-function <https://github.com/tunnckoCore/is-async-function>
  *
- * Copyright (c) 2015 Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Copyright (c) 2015-2016 Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
  * Released under the MIT license.
  */
 
